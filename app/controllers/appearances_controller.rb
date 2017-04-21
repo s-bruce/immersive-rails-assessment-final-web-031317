62432356aab4023cd489972892912779da92d984
github username <github email address>
2017-04-21 10:42:05 -0400

    Done.

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -6,6 +6,7 @@ class AppearancesController < ApplicationController
 
   def create
     @appearance = Appearance.new(appearance_params)
+    @appearance.user_id = current_user.id
     if @appearance.save
       redirect_to @appearance.episode
     else
@@ -13,10 +14,25 @@ class AppearancesController < ApplicationController
     end
   end
 
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+    if @appearance.update(appearance_params)
+      redirect_to episode_path(@appearance.episode)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def appearance_params
     params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    # I did not add a param for user_id since I decided not to include
+    # that field/attribute in the forms
   end
 
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,5 +1,8 @@
 class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
+  belongs_to :user
+
+  validates :rating, inclusion: { in: (1..5), message: "rating must be between 1 and 5" }
   
 end
